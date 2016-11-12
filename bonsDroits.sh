1cf86f956f6507ab1224f47439c385ce40ed6e19
Georges Khaznadar <georgesk@debian.org>
2016-11-12 22:07:28 +0100

    Rendu plus général le script d'ajustement des droits

--- bonsDroits.sh
+++ bonsDroits.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-if [ "$(id -u)" != 0 ]; then
-    echo "il faut être super-utilisateur"
-    exit 1
-fi
-
 # l'utilisateur $specialuser est autorisé à bricoler
-specialuser=georgesk
+specialuser=$(id -un)
 
-chmod 2775 .
-chown $specialuser:www-data .
+sudo chmod 2775 .
+sudo chown $specialuser:www-data .
 
-chown www-data:www-data edt.db
-setfacl -m u:$specialuser:rw edt.db
+sudo chown www-data:www-data edt.db
+sudo setfacl -m u:$specialuser:rw edt.db
